16cb573d6d99902aa6b4712ded8452c274be6cc5
Bryce Kahle <bkahle@gmail.com>
2017-07-31 15:00:56 -0400

    Check if email is sent before validating

--- api/user.go
+++ api/user.go
@@ -6,8 +6,8 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/sirupsen/logrus"
 	"github.com/netlify/gotrue/models"
+	"github.com/sirupsen/logrus"
 )
 
 // UserUpdateParams parameters for updating a user
@@ -83,23 +83,25 @@ func (a *API) UserUpdate(ctx context.Context, w http.ResponseWriter, r *http.Req
 	}
 
 	var sendChangeEmailVerification bool
-	if err = a.mailer.ValidateEmail(params.Email); err == nil {
-		exists, err := a.db.IsDuplicatedEmail(params.Email, user.Aud)
-		if err != nil {
-			InternalServerError(w, err.Error())
+	if params.Email != "" {
+		if err = a.mailer.ValidateEmail(params.Email); err == nil {
+			exists, err := a.db.IsDuplicatedEmail(params.Email, user.Aud)
+			if err != nil {
+				InternalServerError(w, err.Error())
+				return
+			}
+
+			if exists {
+				UnprocessableEntity(w, "Email address already registered by another user")
+				return
+			}
+
+			user.GenerateEmailChange(params.Email)
+			sendChangeEmailVerification = true
+		} else {
+			UnprocessableEntity(w, "Unable to verify new email address: "+err.Error())
 			return
 		}
-
-		if exists {
-			UnprocessableEntity(w, "Email address already registered by another user")
-			return
-		}
-
-		user.GenerateEmailChange(params.Email)
-		sendChangeEmailVerification = true
-	} else {
-		UnprocessableEntity(w, "Unable to verify new email address: "+err.Error())
-		return
 	}
 
 	logrus.Debugf("Checking params for token %v", params)
