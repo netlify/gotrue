bd2b55eb3c4bb8b34052fa6747c6a241331000e1
Bryce Kahle <bkahle@gmail.com>
2017-08-17 12:17:57 -0400

    Add create and get tests for instances

---
+++ api/instance_test.go
@@ -0,0 +1,115 @@
+package api
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/pborman/uuid"
+
+	"github.com/netlify/gotrue/conf"
+	"github.com/netlify/gotrue/models"
+	"github.com/netlify/gotrue/storage/dial"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
+)
+
+const testUUID = "11111111-1111-1111-1111-111111111111"
+const netlifySecret = "netlifysecret"
+
+type InstanceTestSuite struct {
+	suite.Suite
+	API *API
+}
+
+func (ts *InstanceTestSuite) SetupTest() {
+	globalConfig, err := conf.LoadGlobalFromFile("config.test.json")
+	require.NoError(ts.T(), err)
+	globalConfig.NetlifySecret = netlifySecret
+	globalConfig.MultiInstanceMode = true
+	db, err := dial.Dial(globalConfig)
+	require.NoError(ts.T(), err)
+
+	api := NewAPI(globalConfig, db)
+	ts.API = api
+
+	// Cleanup existing user
+	i, err := ts.API.db.GetInstanceByUUID(testUUID)
+	if err == nil {
+		require.NoError(ts.T(), api.db.DeleteInstance(i))
+	}
+}
+
+func (ts *InstanceTestSuite) TestCreate() {
+	// Request body
+	var buffer bytes.Buffer
+	require.NoError(ts.T(), json.NewEncoder(&buffer).Encode(map[string]interface{}{
+		"uuid":     testUUID,
+		"site_url": "https://example.netlify.com",
+		"config": map[string]interface{}{
+			"jwt": map[string]interface{}{
+				"secret": "testsecret",
+			},
+		},
+		"contexts": map[string]interface{}{
+			"production": map[string]interface{}{
+				"jwt": map[string]interface{}{
+					"aud": "prodaud",
+				},
+			},
+		},
+	}))
+
+	// Setup request
+	req := httptest.NewRequest(http.MethodPost, "http://localhost/instances", &buffer)
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", "Bearer "+netlifySecret)
+
+	// Setup response recorder
+	w := httptest.NewRecorder()
+
+	ts.API.handler.ServeHTTP(w, req)
+	require.Equal(ts.T(), w.Code, http.StatusCreated)
+	resp := models.Instance{}
+	require.NoError(ts.T(), json.NewDecoder(w.Body).Decode(&resp))
+	assert.NotNil(ts.T(), resp.BaseConfig)
+	assert.NotNil(ts.T(), resp.Contexts)
+	assert.Equal(ts.T(), "prodaud", resp.Contexts["production"].JWT.Aud)
+
+	i, err := ts.API.db.GetInstanceByUUID(testUUID)
+	require.NoError(ts.T(), err)
+	assert.NotNil(ts.T(), i.BaseConfig)
+	assert.NotNil(ts.T(), i.Contexts)
+	assert.Equal(ts.T(), "prodaud", i.Contexts["production"].JWT.Aud)
+}
+
+func (ts *InstanceTestSuite) TestGet() {
+	instanceID := uuid.NewRandom().String()
+	err := ts.API.db.CreateInstance(&models.Instance{
+		ID:   instanceID,
+		UUID: testUUID,
+		BaseConfig: &conf.Configuration{
+			JWT: conf.JWTConfiguration{
+				Secret: "testsecret",
+			},
+		},
+	})
+	require.NoError(ts.T(), err)
+
+	req := httptest.NewRequest(http.MethodGet, "http://localhost/instances/"+instanceID, nil)
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", "Bearer "+netlifySecret)
+
+	w := httptest.NewRecorder()
+	ts.API.handler.ServeHTTP(w, req)
+	require.Equal(ts.T(), w.Code, http.StatusOK)
+	resp := models.Instance{}
+	require.NoError(ts.T(), json.NewDecoder(w.Body).Decode(&resp))
+}
+
+func TestInstance(t *testing.T) {
+	suite.Run(t, new(InstanceTestSuite))
+}
