e41b01c2b50d3bf87d912cc76ee414ca610caad1
Bryce Kahle <bkahle@gmail.com>
2017-09-04 12:01:56 -0400

    Ensure last sign in time is updated whenever a token is issued

--- api/external.go
+++ api/external.go
@@ -147,8 +147,6 @@ func (a *API) internalExternalProviderCallback(w http.ResponseWriter, r *http.Re
 		user.Confirm()
 	}
 
-	now := time.Now()
-	user.LastSignInAt = &now
 	token, err := a.issueRefreshToken(ctx, user)
 	if err != nil {
 		return oauthError("server_error", err.Error())
