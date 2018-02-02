6f44e84a9c79e656671848a72c595281129c7bbf
Bryce Kahle <bkahle@gmail.com>
2018-02-02 15:56:24 -0500

    Tweaks to migrations based on real-world testing

--- migrations/20180103212743_json_user_metadata.down.sql
+++ migrations/20180103212743_json_user_metadata.down.sql
@@ -1,3 +1,3 @@
 ALTER TABLE `{{ index .Options "Namespace" }}users` 
-CHANGE COLUMN `raw_app_meta_data` `raw_app_meta_data` varchar(255) DEFAULT NULL ,
-CHANGE COLUMN `raw_user_meta_data` `raw_user_meta_data` varchar(255) DEFAULT NULL ;
+CHANGE COLUMN `raw_app_meta_data` `raw_app_meta_data` text DEFAULT NULL ,
+CHANGE COLUMN `raw_user_meta_data` `raw_user_meta_data` text DEFAULT NULL ;
--- migrations/20180125194653_add_refresh_tokens_updated_at.up.sql
+++ migrations/20180125194653_add_refresh_tokens_updated_at.up.sql
@@ -1,1 +1,2 @@
 ALTER TABLE `{{ index .Options "Namespace" }}refresh_tokens` ADD `updated_at` timestamp NULL DEFAULT NULL AFTER `created_at`;
+UPDATE `{{ index .Options "Namespace" }}refresh_tokens` SET `updated_at` = `created_at` WHERE `updated_at` IS NULL;
