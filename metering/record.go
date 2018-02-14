519379d4efded0c8af10cd81109f620805cb43da
Ryan Neal <ry.neal@gmail.com>
2018-02-14 13:42:35 -0800

    stop logging the same domain over and over

--- metering/record.go
+++ metering/record.go
@@ -1,18 +1,15 @@
 package metering
 
 import (
-	"net/http"
-
 	uuid "github.com/satori/go.uuid"
 	"github.com/sirupsen/logrus"
 )
 
 var logger = logrus.StandardLogger().WithField("metering", true)
 
-func RecordLogin(r *http.Request, loginType string, userID, instanceID uuid.UUID) {
+func RecordLogin(loginType string, userID, instanceID uuid.UUID) {
 	logger.WithFields(logrus.Fields{
 		"action":       "login",
-		"domain":       r.Host,
 		"login_method": loginType,
 		"instance_id":  instanceID.String(),
 		"user_id":      userID.String(),
