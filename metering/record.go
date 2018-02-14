bff20aa9f0ca28a178c203b4779c61d41f71fa47
Ryan Neal <ry.neal@gmail.com>
2018-02-14 11:44:44 -0800

    actually log it out

--- metering/record.go
+++ metering/record.go
@@ -11,9 +11,10 @@ var logger = logrus.StandardLogger().WithField("metering", true)
 
 func RecordLogin(r *http.Request, loginType string, userID, instanceID uuid.UUID) {
 	logger.WithFields(logrus.Fields{
-		"action":      "login",
-		"domain":      r.Host,
-		"instance_id": instanceID.String(),
-		"user_id":     userID.String(),
+		"action":       "login",
+		"domain":       r.Host,
+		"login_method": loginType,
+		"instance_id":  instanceID.String(),
+		"user_id":      userID.String(),
 	}).Info("Login")
 }
