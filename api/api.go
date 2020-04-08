e18de44eaf06445d96109b4be39c7f48c9ab652b
Vivian Brown <vivian@netlify.com>
2020-04-07 18:52:28 -0700

    Use new chi version correctly

--- api/api.go
+++ api/api.go
@@ -180,7 +180,7 @@ func NewAPIWithVersion(ctx context.Context, globalConfig *conf.GlobalConfigurati
 		AllowCredentials: true,
 	})
 
-	api.handler = corsHandler.Handler(chi.ServerBaseContext(r, ctx))
+	api.handler = corsHandler.Handler(chi.ServerBaseContext(ctx, r))
 	return api
 }
 
