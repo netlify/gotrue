445797b88ff4cb0bbb32e88b1e25ecea6d98351c
Jordan Levet <jlevet@coltbuilders.com>
2020-12-22 11:29:49 -0700

    Adding Azure as a external provider

--- api/external.go
+++ api/external.go
@@ -308,6 +308,8 @@ func (a *API) Provider(ctx context.Context, name string) (provider.Provider, err
 		return provider.NewGoogleProvider(config.External.Google)
 	case "facebook":
 		return provider.NewFacebookProvider(config.External.Facebook)
+	case "azure":
+	  return provider.NewAzureProvider(config.External.Azure)
 	case "saml":
 		return provider.NewSamlProvider(config.External.Saml, a.db, getInstanceID(ctx))
 	default:
