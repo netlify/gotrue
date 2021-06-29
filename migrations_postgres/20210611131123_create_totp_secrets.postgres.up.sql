7e096b7096f0b73c58899299a4165b25083f3f9e
kangmingtay <kang.ming1996@gmail.com>
2021-06-29 16:32:28 +0800

    update migrations

--- migrations_postgres/20210611131123_create_totp_secrets.postgres.up.sql
+++ migrations_postgres/20210611131123_create_totp_secrets.postgres.up.sql
@@ -1,15 +1,15 @@
 -- auth.totp_secrets definition
 
-CREATE TABLE IF NOT EXISTS auth.totp_secrets (
+CREATE TABLE IF NOT EXISTS auth.totp_auth(
     instance_id uuid NULL,
     id bigserial NOT NULL,
     user_id uuid NOT NULL UNIQUE,
-    encrypted_secret bytea NULL,
+    encrypted_url bytea NULL,
     otp_last_requested_at timestamptz NULL,
     created_at timestamptz NULL,
 	updated_at timestamptz NULL,
     CONSTRAINT totp_secrets_pkey PRIMARY KEY (id)
 );
-CREATE INDEX totp_secrets_instance_id_idx ON auth.totp_secrets USING btree (instance_id);
-CREATE INDEX totp_secrets_instance_id_user_id_idx ON auth.totp_secrets USING btree (instance_id, user_id);
-comment on table auth.totp_secrets is 'Auth: Store of totp secrets used to generate otp once they expire.';
+CREATE INDEX totp_auth_instance_id_idx ON auth.totp_auth USING btree (instance_id);
+CREATE INDEX totp_auth_instance_id_user_id_idx ON auth.totp_auth USING btree (instance_id, user_id);
+comment on table auth.totp_auth is 'Auth: Store of totp secrets used to generate otp once they expire.';
