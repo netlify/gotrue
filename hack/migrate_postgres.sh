cae937e9a8b6dcc60bc5d174e0008a8f9255227a
Kang Ming <kang.ming1996@gmail.com>
2021-07-10 04:35:31 +0800

    chore: mv 20210710035447_alter_users to /migrations

--- hack/migrate_postgres.sh
+++ hack/migrate_postgres.sh
@@ -7,6 +7,6 @@ DATABASE="$DIR/database.yml"
 
 export GOTRUE_DB_DRIVER="postgres"
 export GOTRUE_DB_DATABASE_URL="postgres://postgres:root@localhost:5432/$DB_ENV?sslmode=disable"
-export GOTRUE_DB_MIGRATIONS_PATH=$DIR/../migrations_postgres
+export GOTRUE_DB_MIGRATIONS_PATH=$DIR/../migrations
 
 go run main.go migrate -c $DIR/test.env
