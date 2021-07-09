27c0f528b32b84e2c4f0772ce2758309c6411ff0
Kang Ming <kang.ming1996@gmail.com>
2021-07-10 03:48:11 +0800

    chore: add init sql for local dev

--- hack/postgresd.sh
+++ hack/postgresd.sh
@@ -10,4 +10,5 @@ docker run --name gotrue_postgresql \
 	-e POSTGRES_PASSWORD=root \
 	-e POSTGRES_DB=postgres \
 	--volume postgres_data:/var/lib/postgresql/data \
+	--volume "$(pwd)"/hack/init_postgres.sql:/docker-entrypoint-initdb.d/init.sql \
 	-d postgres:13
