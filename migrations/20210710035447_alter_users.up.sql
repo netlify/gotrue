a843b8965cb46ebb718d5281e1c149d10c99063e
Kang Ming <kang.ming1996@gmail.com>
2021-07-26 11:31:28 +0800

    fix: update migration

--- migrations/20210710035447_alter_users.up.sql
+++ migrations/20210710035447_alter_users.up.sql
@@ -1,11 +1,11 @@
 -- alter user schema
 
 ALTER TABLE auth.users 
-ADD COLUMN phone VARCHAR(15) NULL UNIQUE DEFAULT NULL,
-ADD COLUMN phone_confirmed_at timestamptz NULL DEFAULT NULL,
-ADD COLUMN phone_change VARCHAR(15) NULL DEFAULT '',
-ADD COLUMN phone_change_token VARCHAR(255) NULL DEFAULT '',
-ADD COLUMN phone_change_sent_at timestamptz NULL DEFAULT NULL;
+ADD COLUMN IF NOT EXISTS phone VARCHAR(15) NULL UNIQUE DEFAULT NULL,
+ADD COLUMN IF NOT EXISTS phone_confirmed_at timestamptz NULL DEFAULT NULL,
+ADD COLUMN IF NOT EXISTS phone_change VARCHAR(15) NULL DEFAULT '',
+ADD COLUMN IF NOT EXISTS phone_change_token VARCHAR(255) NULL DEFAULT '',
+ADD COLUMN IF NOT EXISTS phone_change_sent_at timestamptz NULL DEFAULT NULL;
 
 ALTER TABLE auth.users
 RENAME COLUMN confirmed_at TO email_confirmed_at;
