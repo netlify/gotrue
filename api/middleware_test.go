81908f2797bb4e86933f47e6be4bf3dded57d720
Kang Ming <kang.ming1996@gmail.com>
2021-08-30 12:38:08 +0800

    test: add test captcha middleware

--- api/middleware_test.go
+++ api/middleware_test.go
@@ -1,11 +1,84 @@
 package api
 
 import (
+	"bytes"
 	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
+	"github.com/gofrs/uuid"
+	"github.com/netlify/gotrue/conf"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
 )
+
+const (
+	secret   string = "0x0000000000000000000000000000000000000000"
+	response string = "10000000-aaaa-bbbb-cccc-000000000001"
+)
+
+type MiddlewareTestSuite struct {
+	suite.Suite
+	API    *API
+	Config *conf.Configuration
+
+	instanceID uuid.UUID
+}
+
+func TestHCaptcha(t *testing.T) {
+	api, config, instanceID, err := setupAPIForTestForInstance()
+	require.NoError(t, err)
+
+	ts := &MiddlewareTestSuite{
+		API:        api,
+		Config:     config,
+		instanceID: instanceID,
+	}
+	defer api.db.Close()
+
+	suite.Run(t, ts)
+}
+
+func (ts *MiddlewareTestSuite) TestVerifyCaptchaValid() {
+	var buffer bytes.Buffer
+	require.NoError(ts.T(), json.NewEncoder(&buffer).Encode(map[string]interface{}{
+		"email":          "test@example.com",
+		"password":       "secret",
+		"hcaptcha_token": response,
+	}))
+
+	ts.Config.Security.Captcha.Enabled = true
+	ts.Config.Security.Captcha.Provider = "hcaptcha"
+	ts.Config.Security.Captcha.Secret = secret
+
+	req := httptest.NewRequest(http.MethodPost, "http://localhost", &buffer)
+	req.Header.Set("Content-Type", "application/json")
+	beforeCtx, err := WithInstanceConfig(req.Context(), ts.Config, ts.instanceID)
+	require.NoError(ts.T(), err)
+
+	req = req.WithContext(beforeCtx)
+
+	w := httptest.NewRecorder()
+
+	afterCtx, err := ts.API.verifyCaptcha(w, req)
+	require.NoError(ts.T(), err)
+
+	body, err := ioutil.ReadAll(req.Body)
+
+	// re-initialize buffer
+	require.NoError(ts.T(), json.NewEncoder(&buffer).Encode(map[string]interface{}{
+		"email":          "test@example.com",
+		"password":       "secret",
+		"hcaptcha_token": response,
+	}))
+
+	// check if body is the same
+	require.Equal(ts.T(), body, buffer.Bytes())
+	require.Equal(ts.T(), afterCtx, beforeCtx)
+}
 
 func TestFunctionHooksUnmarshalJSON(t *testing.T) {
 	tests := []struct {
