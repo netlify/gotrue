87f068daf6c969c6f862f9de2f81da992d514a46
Marcus Weiner <marcus@netlify.com>
2021-11-15 18:23:35 +0100

    Upgrade some more dependencies (#308)
    * Upgrade pop to use latest v5
    
    * Force the upgrade a transient dependency
    
    * Fix func call
    
    * Remove unused code

--- storage/dial.go
+++ storage/dial.go
@@ -2,7 +2,6 @@ package storage
 
 import (
 	"net/url"
-	"reflect"
 
 	_ "github.com/GoogleCloudPlatform/cloudsql-proxy/proxy/dialers/mysql"
 	_ "github.com/go-sql-driver/mysql"
@@ -62,13 +61,9 @@ func (c *Connection) Transaction(fn func(*Connection) error) error {
 
 func getExcludedColumns(model interface{}, includeColumns ...string) ([]string, error) {
 	sm := &pop.Model{Value: model}
-	st := reflect.TypeOf(model)
-	if st.Kind() == reflect.Ptr {
-		st = st.Elem()
-	}
 
 	// get all columns and remove included to get excluded set
-	cols := columns.ForStructWithAlias(model, sm.TableName(), sm.As)
+	cols := columns.ForStructWithAlias(model, sm.TableName(), sm.As, sm.IDField())
 	for _, f := range includeColumns {
 		if _, ok := cols.Cols[f]; !ok {
 			return nil, errors.Errorf("Invalid column name %s", f)
--- tools.go
+++ tools.go
@@ -3,6 +3,6 @@
 package tools
 
 import (
-	_ "github.com/gobuffalo/pop/soda"
+	_ "github.com/gobuffalo/pop/v5/soda"
 	_ "golang.org/x/lint/golint"
 )
